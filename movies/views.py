e30344e43ed03e63b3478591c5798b552c8f447e
Joao Nicolas Retti <jnretti@usp.br>
2024-09-16 15:44:26 -0300

    Adicionar a vista dos detalhes

--- movies/views.py
+++ movies/views.py
@@ -3,9 +3,8 @@ from django.http import HttpResponse
 from .temp_data import movie_data
 
 def detail_movie(request, movie_id):
-    movie = movie_data[movie_id - 1]
-    return HttpResponse(
-        f'Detalhes do filme {movie["name"]} ({movie["release_year"]})')
+    context = {'movie': movie_data[movie_id - 1]}
+    return render(request, 'movies/detail.html', context)
 
 def list_movies(request):
     context = {"movie_list": movie_data}
